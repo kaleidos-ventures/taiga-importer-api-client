b6d1ec46413b5b958155d99f78415e2f101fbe97
Iván López <ivan.lopez@kaleidos.net>
2014-09-29 09:22:48 +0200

    Add wikipage documentation

--- src/test/groovy/net/kaleidos/taiga/testdata/WikipageData.groovy
+++ src/test/groovy/net/kaleidos/taiga/testdata/WikipageData.groovy
@@ -6,10 +6,12 @@ import net.kaleidos.domain.Wikipage
 trait WikipageData {
 
     Wikipage buildBasicWikipage(Project project) {
+        // tag::createWikipage[]
         new Wikipage()
             .setProject(project)
             .setSlug('home')
             .setContent('Lorem ipsum...')
+        // end::createWikipage[]
     }
 
 }
