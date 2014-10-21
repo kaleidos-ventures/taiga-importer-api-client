4d452076949684281de4677b966116cb8a529315
Iván López <ivan.lopez@kaleidos.net>
2014-10-21 10:21:43 +0200

    Add finishedDate to Tasks

--- src/main/groovy/net/kaleidos/taiga/mapper/TaskMapper.groovy
+++ src/main/groovy/net/kaleidos/taiga/mapper/TaskMapper.groovy
@@ -8,19 +8,20 @@ class TaskMapper implements Mapper<Task>, DateConversions {
     @Override
     Map map(Task task) {
         [
-            status      : task.status,
-            subject     : task.subject,
-            description : task.description,
-            project     : task.project.id,
-            ref         : task.ref,
-            owner       : task.owner,
-            created_date: format(task.createdDate),
-            user_story  : task.userStory?.ref,
-            milestone   : task.milestone?.name,
-            attachments : task.attachments.collect { Mappers.map(it) },
-            history     : task.history.collect { Mappers.map(it) },
-            tags        : task.tags,
-            assigned_to : task.assignedTo,
+            status       : task.status,
+            subject      : task.subject,
+            description  : task.description,
+            project      : task.project.id,
+            ref          : task.ref,
+            owner        : task.owner,
+            created_date : format(task.createdDate),
+            finished_date: format(task.finishedDate),
+            user_story   : task.userStory?.ref,
+            milestone    : task.milestone?.name,
+            attachments  : task.attachments.collect { Mappers.map(it) },
+            history      : task.history.collect { Mappers.map(it) },
+            tags         : task.tags,
+            assigned_to  : task.assignedTo,
         ]
     }
 }
