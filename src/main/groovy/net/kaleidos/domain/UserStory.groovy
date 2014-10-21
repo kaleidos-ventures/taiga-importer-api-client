9e2e8a6edd5e886ccfd4fc72ec8f03a154ace1c7
Iván López <ivan.lopez@kaleidos.net>
2014-10-21 14:05:18 +0200

    Add finishedDate to User stories

--- src/main/groovy/net/kaleidos/domain/UserStory.groovy
+++ src/main/groovy/net/kaleidos/domain/UserStory.groovy
@@ -17,6 +17,7 @@ class UserStory {
 
     String owner
     Date createdDate
+    Date finishedDate
 
     List<Attachment> attachments
 
--- src/main/groovy/net/kaleidos/taiga/builder/UserStoryBuilder.groovy
+++ src/main/groovy/net/kaleidos/taiga/builder/UserStoryBuilder.groovy
@@ -4,8 +4,9 @@ import groovy.json.JsonSlurper
 import net.kaleidos.domain.Project
 import net.kaleidos.domain.UserStory
 import net.kaleidos.taiga.common.DateConversions
+import net.kaleidos.taiga.common.SafeJson
 
-class UserStoryBuilder implements TaigaEntityBuilder<UserStory>, DateConversions {
+class UserStoryBuilder implements TaigaEntityBuilder<UserStory>, DateConversions, SafeJson {
 
     UserStory build(Map json, Project project) {
         def userStory = new UserStory()
@@ -18,6 +19,7 @@ class UserStoryBuilder implements TaigaEntityBuilder<UserStory>, DateConversions
             delegate.project = project
             owner = json.owner
             createdDate = parse(json.created_date)
+            finishedDate = parse(nullSafe(json.finish_date))
             attachments = json.attachments.collect { new AttachmentBuilder().build(it, null) }
             history = json.history.collect { new HistoryBuilder().build(it, null) }
             rolePoints = json.role_points.collect { new RolePointBuilder().build(it, null) }
